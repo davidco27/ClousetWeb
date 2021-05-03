294b318d390ab847f944e16d7cb7a14e9cb3fccc
davidco27 <74071922+davidco27@users.noreply.github.com>
2021-05-03 11:27:53 +0200

    Initial commit

---
+++ demo/src/main/java/com/example/closet/ClousetApplication.java
@@ -0,0 +1,13 @@
+package com.example.closet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClousetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClousetApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/closet/ClousetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.closet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClousetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
