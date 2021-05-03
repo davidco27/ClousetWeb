74447e0873ff47eecdaf9cc3dfe13a46c43d4f74
davidco27 <74071922+davidco27@users.noreply.github.com>
2021-05-03 11:37:49 +0200

    hola

--- demo/src/main/java/com/example/closet/ClousetApplication.java
+++ demo/src/main/java/com/example/closet/ClousetApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClousetApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClousetApplication.class, args);
 	}
-
 }
