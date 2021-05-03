ca2a1369853e7992870b81de222c6442ac538f06
davidco27 <74071922+davidco27@users.noreply.github.com>
2021-05-03 12:11:44 +0200

    hola

--- demo/src/main/java/com/example/closet/ClousetApplication.java
+++ demo/src/main/java/com/example/closet/ClousetApplication.java
@@ -1,8 +1,6 @@
 package com.example.closet;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ClousetApplication {
 	public static void main(String[] args) {
---
+++ demo/src/main/java/com/example/closet/controller/RestController.java
@@ -0,0 +1,10 @@
+package com.example.closet.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api")
+public class RestController{
+
+}
