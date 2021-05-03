a293ef9da691bb92da1aa1c9b71d78b90428b1dc
davidco27 <74071922+davidco27@users.noreply.github.com>
2021-05-03 16:23:43 +0200

    hola

---
+++ demo/src/main/java/com/example/closet/config/WebConfig.java
@@ -0,0 +1,31 @@
+package info.jab.microservices.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        jsonConverter.setObjectMapper(objectMapper);
+        jsonConverter.setDefaultCharset(Charset.forName("UTF-8"));
+        return jsonConverter;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+
+}
